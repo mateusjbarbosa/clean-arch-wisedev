6c0a4400fa4dbfaf855c607cbd8cb0a2077c796e
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-04-19 14:39:34 -0300

    chore: ensure no coverage in tests folder

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,10 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/tests/**'
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
