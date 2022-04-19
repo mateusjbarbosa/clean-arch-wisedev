e9df5e474480ba94c1f2490060013a9d6086ec12
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-04-19 13:50:58 -0300

    feat: create user

---
+++ src/entities/user.ts
@@ -0,0 +1,34 @@
+import { Either, left, right } from '../shared/either'
+import { InvalidEmailError } from './errors/invalid-email'
+import { InvalidNameError } from './errors/invalid-name'
+import { UserData } from './user-data'
+import { Email } from './value-objects/email'
+import { Name } from './value-objects/name'
+
+export class User {
+  public readonly name: Name
+  public readonly email: Email
+
+  private constructor (name: Name, email: Email) {
+    this.name = name
+    this.email = email
+  }
+
+  static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
+    const nameOrError = Name.create(userData.name)
+    const emailOrError = Email.create(userData.email)
+
+    if (nameOrError.isLeft()) {
+      return left(new InvalidNameError())
+    }
+
+    if (emailOrError.isLeft()) {
+      return left(new InvalidEmailError())
+    }
+
+    const name = nameOrError.value as Name
+    const email = emailOrError.value as Email
+
+    return right(new User(name, email))
+  }
+}
