ce84a62e147fc4f33be19414ee294b775fb19f5f
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-04-19 00:02:28 -0300

    feat: create email value object

---
+++ src/entities/value-objects/email.ts
@@ -0,0 +1,30 @@
+export class Email {
+  static validate (email: string): boolean {
+    if (!email) {
+      return false
+    }
+    if (email.length > 320) {
+      return false
+    }
+
+    const emailRegex = /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
+    if (!emailRegex.test(email)) {
+      return false
+    }
+
+    const [local, domain] = email.split('@')
+    if (local.length > 64 || local.length === 0) {
+      return false
+    }
+    if (domain.length > 255 || domain.length === 0) {
+      return false
+    }
+
+    const domainParts = domain.split('.')
+    if (domainParts.some(part => part.length > 63)) {
+      return false
+    }
+
+    return true
+  }
+}
