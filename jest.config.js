b65b1f714c5b157ef96e4edce2d776f269b7f854
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-04-18 23:05:32 -0300

    chore: add tests dir in jest config

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,8 @@ module.exports = {
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
-    '<rootDir>/src'
+    '<rootDir>/src',
+    '<rootDir>/tests'
   ],
 
   // The test environment that will be used for testing
