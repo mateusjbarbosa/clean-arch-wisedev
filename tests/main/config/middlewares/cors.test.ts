837c76d2a8f49341f6097b75ee039e2ac47a8164
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-04-25 11:41:41 -0300

    test: apply cors on express app

---
+++ tests/main/config/middlewares/cors.test.ts
@@ -0,0 +1,17 @@
+import { app } from '@/main/config'
+import request from 'supertest'
+
+describe('CORS middleware', () => {
+  it('should enable CORS', async () => {
+    app.get('/tests/cors', (_, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/tests/cors')
+      .expect(200)
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
+  })
+})
