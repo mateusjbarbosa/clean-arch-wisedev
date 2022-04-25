681f033b607d23bb3357f5f2b1f233d49a2834f0
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-04-25 11:48:47 -0300

    feat: apply default content type as json on express app

--- src/main/config/middlewares/setup.ts
+++ src/main/config/middlewares/setup.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bodyParser } from './body-parser'
+import { contentType } from './content-type'
 import { cors } from './cors'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(contentType)
   app.use(cors)
 }
